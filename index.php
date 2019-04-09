6c1b3c6df722073aebf1e744bf8d58e3668b075c
unknown <dylanbourbotte.simplon@gmail.comgit config --global user.name Dylan>
2019-04-09 18:43:15 +0200

    ajout de ma section about

--- index.php
+++ index.php
@@ -12,6 +12,27 @@ include('include/header.php'); // On inclus le header du site
     <div class="row">
         <div class="col-12 text-center"><h2>Qui suis-je ?</h2></div>
     </div>
+    
+<div class="container tripleP">
+    <div class="row text-center">
+        <div class="col-sm">
+            <p class="titleP">Passion</p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque iste libero natus quisquam voluptatum ratione possimus sapiente numquam necessitatibus adipisci delectus aspernatur ex, non deserunt quo dolorum distinctio ad labore?Corporis nihil reprehenderit, voluptate, a, molestiae pariatur excepturi enim perferendis itaque modi tempora natus ullam ratione repellat totam recusandae illum ipsam temporibus aut sit libero? Perferendis quod similique excepturi numquam.
+        </div>
+   
+         <div class="col-sm">
+            <p class="titleP">Formation</p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque iste libero natus quisquam voluptatum ratione possimus sapiente numquam necessitatibus adipisci delectus aspernatur ex, non deserunt quo dolorum distinctio ad labore?Corporis nihil reprehenderit, voluptate, a, molestiae pariatur excepturi enim perferendis itaque modi tempora natus ullam ratione repellat totam recusandae illum ipsam temporibus aut sit libero? Perferendis quod similique excepturi numquam.
+
+        </div>
+    
+        <div class="col-sm">
+            <p class="titleP">Projets</p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque iste libero natus quisquam voluptatum ratione possimus sapiente numquam necessitatibus adipisci delectus aspernatur ex, non deserunt quo dolorum distinctio ad labore?Corporis nihil reprehenderit, voluptate, a, molestiae pariatur excepturi enim perferendis itaque modi tempora natus ullam ratione repellat totam recusandae illum ipsam temporibus aut sit libero? Perferendis quod similique excepturi numquam.
+
+        </div>
+    </div>
+</div>
 </div>
 
 </section>
